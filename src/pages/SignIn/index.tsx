866434563fbfc8ace2380ab0a94adfb3d05acb24
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-01-08 19:53:09 -0300

    Styled SignIn and SignUp pages

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => {}}>Entrar</Button>
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountText>Criar uma conta</CreateAccountText>
+      </CreateAccountButton>
+    </>
+  );
 };
 
 export default SignIn;
